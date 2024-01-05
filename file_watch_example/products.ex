dd81156a48ea15a5ce284b613f24f04e5eac23f2
pascal-chenevas <47719700+pascal-chenevas@users.noreply.github.com>
2024-01-05 15:39:44 +0100

    Create products.ex

---
+++ file_watch_example/products.ex
@@ -0,0 +1,28 @@
+defmodule FileWatchExample.Product do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "products" do
+    field :name, :string
+    field :bar_code, :string
+    field :price, :string
+    field :currency, :string
+
+    timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :bar_code, :price, :currency])
+  end
+
+  def create_product(attrs) do
+    %FileWatchExample.Product{}
+    |> FileWatchExample.Product.changeset(attrs)
+    |> Store.insert(timeout: 1000 * 60)
+  end
+
+  def create_products(products) do
+    Store.insert_all("products", products, returning: [:id])
+  end
+end

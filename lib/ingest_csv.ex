07be7f140a633684a764541bd287272d34f409c9
Dimitar Panayotov <mitko.p@gmail.com>
2024-01-05 22:35:37 +0200

    ingest in chunks without validation

--- lib/ingest_csv.ex
+++ lib/ingest_csv.ex
@@ -5,14 +5,33 @@ defmodule IngestCSV do
 
   NimbleCSV.define(YourCSV, separator: ";")
 
+  def csv_row_to_record([name, bar_code, price, currency], now \\ NaiveDateTime.utc_now()) do
+    [
+      name: name,
+      bar_code: bar_code,
+      price: price,
+      currency: currency,
+      inserted_at: now,
+      updated_at: now
+    ]
+  end
+
   def load(path) do
     path
     |> Path.expand()
-    |> File.stream!(read_ahead: 524_288)
-    |> YourCSV.parse_stream()
-    |> Task.async_stream(fn [name, bar_code, price, currency] ->
-      FileWatchExample.Product.create_product(%{name: name, bar_code: bar_code, price: price, currency: currency})
-    end, max_concurrency: 95, on_timeout: :kill_task, ordered: false)
+    |> File.stream!(read_ahead: 8 * 1024 * 1024)
+    |> YourCSV.parse_stream(skip_headers: false)
+    |> Stream.chunk_every(1000)
+    |> Task.async_stream(
+      fn products ->
+        now = DateTime.utc_now()
+        to_insert = Enum.map(products, fn values -> csv_row_to_record(values, now) end)
+        Store.insert_all(FileWatchExample.Product, to_insert)
+      end,
+      max_concurrency: 100,
+      on_timeout: :kill_task,
+      ordered: false
+    )
     |> Stream.run()
   end
 end

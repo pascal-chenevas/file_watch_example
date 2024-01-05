f9b4d4c8b5ce54fd95c33371a99b5a124049201c
pascal-chenevas <47719700+pascal-chenevas@users.noreply.github.com>
2024-01-05 15:39:56 +0100

    Add files via upload

---
+++ file_watch_example/application.ex
@@ -0,0 +1,23 @@
+defmodule FileWatchExample.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: FileWatchExample.Worker.start_link(arg)
+      # {FileWatchExample.Worker, arg}
+      FileWatcher,
+#      Store
+      Store
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: FileWatchExample.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
